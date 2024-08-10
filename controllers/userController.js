b1ea745fb4c61f5e98cd8fa3a7fd5d1ff82f187c
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-08-11 00:11:28 +0200

    auth fix 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,34 +10,47 @@ const db = mysql.createPool({
     database: 'gifter'
 })
 
+// функция для получения данных по гугл аксес токену
+const getGoogleData = async (access_token) => {
+    const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    });
+
+    const userInfo = await userInfoResponse.json()
+    if (userInfo.error){
+        return ({nickname: null, imgPath: null, role: null, email: null})
+    } else {
+        return(userInfo)
+    }
+}
 
 // получение данных по токену
 const getUserData = async(req, res) => {
     try{        
         const {access_token} = req.body
 
-        // получение данных от гугл по токену
-        const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {
-            headers: {
-                'Authorization': `Bearer ${access_token}`
-            }
-        });
-        
-        const userInfo = await userInfoResponse.json();
-        
+        const userInfo = await getGoogleData(access_token)
+        console.log(userInfo)
+
         // если почта была зарегестрированна, то получаем данные, если нет - то создаём и получаем их
+        if(userInfo.nickname !== null){
 
-        let rows = await db.execute('SELECT * FROM users WHERE email = ?', [userInfo.email])
+            let rows = await db.execute('SELECT * FROM users WHERE email = ?', [userInfo.email])
 
-        if(rows[0].length === 0){
-            await db.execute('INSERT INTO users (nickname, imgPath, role, email) VALUES (?, ?, ?, ?)', [userInfo.name, userInfo.picture, 0, userInfo.email])
-            rows = await db.execute('SELECT * FROM users WHERE email = ?', [userInfo.email])
+            if(rows[0].length === 0){
+                await db.execute('INSERT INTO users (nickname, imgPath, role, email) VALUES (?, ?, ?, ?)', [userInfo.name, userInfo.picture, 0, userInfo.email])
+                rows = await db.execute('SELECT * FROM users WHERE email = ?', [userInfo.email])
+            }
+
+            res.status(200).json(rows[0][0])
+        } else {
+            res.status(200).json({id: null, nickname: null, imgPath: null, role: null, email: null})
         }
 
-        // res.status(200).json(rows[0][0])
     } catch(error){
-        res.status(500).json('ERROR WHILE GETING DATA ' + error)
-        return({id: null, nickname: null, imgPath: null, role: null, email: null})  //НЕ УВЕРЕН ЧТО СРАБОТАЕТ
+        res.status(500).json({massege: "ERROR WHILE GETING DATA " + error})
     }
 
 }
